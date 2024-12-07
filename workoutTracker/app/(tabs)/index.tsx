0865a9fb842c4ac0db092cb0b89d471094133c95
aashtiany <amir.klavuhn-ashtiany@dodiis.mil>
2024-12-07 17:26:07 -0500

    adding sign in form

---
+++ workoutTracker/app/(tabs)/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { Image, StyleSheet, Platform, View } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import LoginForm from '../LoginForm'
+
+export default function HomeScreen() {
+  return (
+     <View style={styles.container}>
+      <LoginForm />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+    container: {
+    flex: 1,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
